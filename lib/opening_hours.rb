20331e467ba0ff87d417716e9d9dbee71885222d
Ian Whitney <whit0694@umn.edu>
2014-06-13 13:01:29 -0500

    Basic requirements tested and implemented.
    This is a terrible implementation, but I wanted to get the tests setup
    and passing before I refactor.

--- lib/opening_hours.rb
+++ lib/opening_hours.rb
@@ -21,8 +21,22 @@ module OpeningHours
         raise ArgumentError unless (1..2).cover?(days.count)
 
         valid_days = %w(Mo Tu We Th Fr Sa Su)
+
+        valid_days = %w(Mo Tu We Th Fr Sa Su)
         days.each do |day|
           raise ArgumentError unless valid_days.include?(day)
+        end
+
+        if day_range.include?('-') && days.count == 2
+          start_day = valid_days.index(days[0])
+          end_day = valid_days.index(days[1])
+          days = valid_days[start_day..end_day]
+        end
+
+        days.each do |day|
+          unless open_hours[valid_days.index(day)]
+            open_hours[valid_days.index(day)] = []
+          end
         end
 
         times = time_range.split('-')
@@ -32,7 +46,37 @@ module OpeningHours
         times.each do |time|
           raise ArgumentError unless time.match(/\d\d:\d\d/)
         end
+
+        days.each do |day|
+          open_time = time_parse(times[0])
+          close_time = time_parse(times[1])
+          open_hours[valid_days.index(day)] << (open_time..close_time)
+        end
       end
+      self
+    end
+
+    def open_at?(date_time)
+      dt = date_time.to_datetime
+      day = dt.wday
+      time = dt.strftime("%H:%M")
+      time = time_parse(time)
+      if open_hours[day]
+        open_hours[day].any? {|x| x.cover?(time) }
+      else
+        false
+      end
+    end
+
+    private
+
+    def open_hours
+      @open_hours ||= {}
+    end
+
+    def time_parse(t)
+      t = Time.parse(t)
+      (t.hour * 60) + t.min
     end
   end
 end
--- spec/opening_hours_spec.rb
+++ spec/opening_hours_spec.rb
@@ -2,13 +2,13 @@ require "rspec/core"
 require_relative "../lib/opening_hours"
 
 describe OpeningHours::OpeningHours do
+  let(:good_param_examples) {[
+    ["Mo-Th 08:00-16:30", "Fr 08:00-16:00"],
+    "Mo-Th 08:00-16:30",
+  ]}
+
   describe "parse" do
     describe "with good params" do
-      let(:good_param_examples) {[
-        ["Mo-Th 08:00-16:30", "Fr 08:00-16:00"],
-        "Mo-Th 08:00-16:30",
-      ]}
-
       it "returns an instance of OpeningHours" do
         good_param_examples.each do |good_params|
           it = OpeningHours::OpeningHours.parse(good_params)
@@ -41,6 +41,71 @@ describe OpeningHours::OpeningHours do
         bad_param_examples.each do |bad_params|
           expect {OpeningHours::OpeningHours.parse(bad_params)}.to raise_error(ArgumentError)
         end
+      end
+    end
+  end
+
+  describe "open_at?" do
+    describe "params" do
+      it "requires something that can be parsed with strftime" do
+        param = DateTime.now
+        expect(param).to receive(:to_datetime) { DateTime.now }
+        it = OpeningHours::OpeningHours.parse(good_param_examples.sample)
+        it.open_at?(param)
+      end
+    end
+
+    describe "date/time within opening hours" do
+      before do
+        @it = OpeningHours::OpeningHours.parse(["Mo-Th 08:00-16:30", "Fr 08:00-16:00"])
+      end
+
+      it "returns true if date and time is the first mintue of open hours" do
+        time = DateTime.parse("Monday, June 16 08:00")
+        expect(@it.open_at?(time)).to be_truthy
+      end
+
+      it "returns true if date and time is the last minute of open hours" do
+        time = DateTime.parse("Monday, June 16 16:30")
+        expect(@it.open_at?(time)).to be_truthy
+      end
+
+      it "returns true for a time for each day in range" do
+        %w(Tuesday Wednesday Thursday).each do |day|
+          time = DateTime.parse("#{day}, June 16 16:30")
+          expect(@it.open_at?(time)).to be_truthy
+        end
+      end
+
+      it "returns true for a time for a single day" do
+        time = DateTime.parse("Friday, June 16 12:30")
+        expect(@it.open_at?(time)).to be_truthy
+      end
+    end
+
+    describe "date/time outside opening hours" do
+      before do
+        @it = OpeningHours::OpeningHours.parse(["Mo-Th 08:00-16:30", "Fr 08:00-16:00"])
+      end
+
+      it "returns false if the time is before open hours" do
+        time = DateTime.parse("Monday, June 16 07:59")
+        expect(@it.open_at?(time)).to be_falsey
+      end
+
+      it "returns false if the time is after open hours" do
+        time = DateTime.parse("Monday, June 16 16:31")
+        expect(@it.open_at?(time)).to be_falsey
+      end
+
+      it "returns false if the time is in open hours, but on an unspecified day" do
+        time = DateTime.parse("Saturday, June 21 12:30")
+        expect(@it.open_at?(time)).to be_falsey
+      end
+
+      it "returns false if the time is in open hours for a different day, but not this day" do
+        time = DateTime.parse("Friday, June 20 16:01")
+        expect(@it.open_at?(time)).to be_falsey
       end
     end
   end
